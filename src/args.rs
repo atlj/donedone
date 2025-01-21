d8f3fbbc75216b70f22fd5da5d0a60aee64e9941
Burak Güner <burak.guner@callstack.com>
2025-01-21 23:05:10 +0300

    add some basic args

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,23 @@
-use clap::Parser;
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
-pub struct Args {}
+pub struct Args {
+    #[command(subcommand)]
+    commands: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    /// Add a new item
+    Add {
+        /// Path to the file
+        file_path: PathBuf,
+        /// Which line to add the todo to
+        line: usize,
+    },
+    /// Sets up dndn in a way to not affect git
+    Setup {},
+}
