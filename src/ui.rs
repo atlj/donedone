9a9197ec513be9ecb3665aa93d9ca3505ca23a4b
Burak Güner <burak.guner@callstack.com>
2025-01-25 17:08:07 +0300

    basic ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,10 @@
-use std::io::{self, stdout, Write};
+use std::io::{self, stdout, Stdout, Write};
 
 use crossterm::{
-    style::Print,
-    terminal::{Clear, ClearType},
+    cursor,
+    event::Event,
+    style::{self, Print, PrintStyledContent},
+    terminal::{self, enable_raw_mode},
     QueueableCommand,
 };
 
@@ -11,9 +13,33 @@ use crate::entry::Entry;
 pub fn display_entries(entries: &Vec<Entry>) -> Result<(), io::Error> {
     let mut stdout = stdout();
     stdout
-        .queue(Clear(ClearType::All))?
-        .queue(Print("Testing 123".to_string()))?;
+        .queue(terminal::EnterAlternateScreen)?
+        .queue(terminal::Clear(terminal::ClearType::All))?
+        .queue(cursor::Hide)?;
     stdout.flush()?;
+
+    render(&mut stdout, entries);
+    stdout.flush();
+
+    loop {
+        match crossterm::event::read()? {
+            Event::Resize(y, x) => {}
+            _ => {}
+        }
+    }
+
+    return Ok(());
+}
+
+fn render(stdout: &mut Stdout, entries: &Vec<Entry>) -> Result<(), io::Error> {
+    let (x_size, y_size) = terminal::size()?;
+    let comment = entries.get(0).unwrap().comment.clone().unwrap();
+
+    stdout
+        .queue(cursor::SavePosition)?
+        .queue(cursor::MoveToRow(y_size / 2))?
+        .queue(cursor::MoveToColumn((x_size - (comment.len() as u16)) / 2))?
+        .queue(style::Print(comment))?;
 
     return Ok(());
 }
